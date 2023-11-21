1a772d018707e6aafd99e02c8ac0f84e53f3941d
Huy Trần Lê <tranlehuy0407@gmail.com>
2023-11-21 12:40:09 +0700

    test

--- App.js
+++ App.js
@@ -1,17 +1,112 @@
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Screen01 from "./components/Home";
-import Screen02 from "./components/Detail";
+import React, { useEffect, useState } from "react";
+import { ScrollView, ImageBackground } from "react-native";
+import ForecastSearch from "./components/ForecastSearch";
+import CurrentForecast from "./components/CurrentForecast";
+import DailyForecast from "./components/DailyForecast";
+import styled from "styled-components/native";
+import config from "./config";
+import bgImg from "./assets/4.png";
 
-const Stack = createStackNavigator();
+const App = () => {
+  const [toggleSearch, setToggleSearch] = useState("city");
+  const [city, setCity] = useState("Toronto");
+  const [postalCode, setPostalCode] = useState("L4W1S9");
+  const [lat, setLat] = useState(43.6532);
+  const [long, setLong] = useState(-79.3832);
+  const [weather, setWeather] = useState({});
 
-export default function App() {
+  const controller = new AbortController();
+  const signal = controller.signal;
+  const Api_Key = '8d2de98e089f1c28e1a22fc19a24ef04';
+
+  //fetch lat long by city
+  const fetchLatLongHandler = () => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_Key}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setLat(data.coord.lat);
+        setLong(data.coord.lon);
+      });
+  };
+
+  //fetch lat long by postal code/zip since OpenWeather Api only accepts zips
+  const fetchByPostalHandler = () => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?key=${config.GOOGLE_KEY}&components=postal_code:${postalCode}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setLat(data.results[0].geometry.location.lat);
+        setLong(data.results[0].geometry.location.lng);
+      });
+  };
+
+  //updates the weather when lat long changes
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely&units=metric&appid=${Api_Key}`,
+      { signal }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeather(data);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+    return () => controller.abort();
+  }, [lat, long]);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="1">
-        <Stack.Screen name="1" component={Screen01} options={{ headerShown: false }} />
-        <Stack.Screen name="2" component={Screen02} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Container>
+      <ScrollView>
+        <ImageBackground source={bgImg} style={{ width: "100%", height: "100%" }}>
+          <ForecastSearch
+            city={city}
+            setCity={setCity}
+            fetchLatLongHandler={fetchLatLongHandler}
+            toggleSearch={toggleSearch}
+            setToggleSearch={setToggleSearch}
+            fetchByPostalHandler={fetchByPostalHandler}
+            setPostalCode={setPostalCode}
+            postalCode={postalCode}
+          />
+          <CurrentForecast currentWeather={weather} timezone={weather.timezone} />
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={{ flex: 1 }}>
+            <FutureForecastContainer>
+              {weather.daily ? (
+                weather.daily.map((day, index) => {
+                  if (index !== 0) {
+                    return <DailyForecast key={day.dt} day={day} index={index} />;
+                  }
+                })
+              ) : (
+                <NoWeather>No Weather to show</NoWeather>
+              )}
+            </FutureForecastContainer>
+          </ScrollView>
+        </ImageBackground>
+      </ScrollView>
+    </Container>
   );
-}
+};
+
+const Container = styled.View`
+  flex: 1;
+  background-color: dodgerblue;
+`;
+
+const NoWeather = styled.Text`
+  text-align: center;
+  color: white;
+`;
+
+const FutureForecastContainer = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default App;
